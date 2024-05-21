b0ca6d86e2d171836e48e517bab8aa4c95b0032b
thomas giroud <thomasgiroud07@gmail.com>
2024-05-21 09:36:35 +0200

    Record Product

--- Backend/backend/src/App.java
+++ Backend/backend/src/App.java
@@ -1,6 +1,9 @@
 import database.PolyBayDatabase;
+import models.Product;
 public class App {
     public static void main(String[] args) throws Exception {
         PolyBayDatabase polyBayDatabase = new PolyBayDatabase();
+        Product product  = new Product(1, "toto", "NONO", 0.5f);
+        System.out.println(product.id());
     }
 }
---
+++ Backend/backend/src/models/Product.java
@@ -0,0 +1,9 @@
+package models;
+
+public record Product(
+    int id,
+    String name,
+    String owner, 
+    float bid
+
+){}
