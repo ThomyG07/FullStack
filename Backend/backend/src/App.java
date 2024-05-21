88265cac1f509b85aec2aecafd34639699c767e9
thomas giroud <thomasgiroud07@gmail.com>
2024-05-21 09:14:21 +0200

    Creation de la Classe de PolyBay

--- Backend/backend/src/App.java
+++ Backend/backend/src/App.java
@@ -1,5 +1,6 @@
+import database.PolyBayDatabase;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        PolyBayDatabase polyBayDatabase = new PolyBayDatabase();
     }
 }
---
+++ Backend/backend/src/database/PolyBayDatabase.java
@@ -0,0 +1,14 @@
+package database;
+
+import java.sql.SQLException;
+
+public class PolyBayDatabase extends MySQLDatabase{
+
+    public PolyBayDatabase() throws SQLException
+    {
+
+        super("localhost",3306, "poly_bay","Toto", "test");
+    }
+    
+
+}
